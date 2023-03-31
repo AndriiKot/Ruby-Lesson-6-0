ce3b77241e623d280c1d4ca277c9942fa7bf9b02
AndriiKot <a_kocyuba@ukr.net>
2023-03-31 15:23:14 +0200

    add test methods hi and self.hi

--- ruby_singleton_methods.rb
+++ ruby_singleton_methods.rb
@@ -1,1 +1,27 @@
 ﻿require 'selenium-webdriver'
+
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def Animal.find(term = '')
+    driver = Selenium::WebDriver :chrom
+  end
+
+  def self.hi = puts "Hi, my name is #{name}!"
+  def hi      = puts "Hi, my name is #{name}!"
+
+end
+
+class Cat < Animal
+end
+
+cat1 = Cat.new "Mr. Buttons"
+cat1.hi                      # "Hi, my name is Mr. Buttons!"
+cat1.class.hi                # "Hi, my name is Cat!"
+
+
+  
